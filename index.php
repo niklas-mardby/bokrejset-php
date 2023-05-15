ed7d9780de3f3865e10d7058fdf6df4e8f0aeee5
Niklas Mårdby <niklas.mardby@gmail.com>
2023-05-15 11:40:56 +0200

    Some scaffolding

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'partials/config.php';
+require 'classes/db.php';
+
+$db = new DB();
+
+
+include 'partials/header.php';
+
+// våran apps vyer här
+
+include 'partials/footer.php';
