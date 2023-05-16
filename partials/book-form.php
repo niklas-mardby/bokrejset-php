1dc356451876d0651f6f0b292ae22dc22c9c0c4e
Niklas Mårdby <niklas.mardby@gmail.com>
2023-05-16 13:09:54 +0200

    Form cleanup

--- partials/book-form.php
+++ partials/book-form.php
@@ -1,10 +1,11 @@
 <form action="form-handlers/book-form-handler.php" method="post">
     <div>
-        <label for="title">Title: </label><input type="text" name="title" id="title">
+        <label for="title">Titel: </label>
+        <input type="text" name="title" id="title">
     </div>
     <div>
-        <label for="year">Year published: </label>
+        <label for="year">Publicerad år: </label>
         <input type="text" name="year" id="year">
     </div>
-    <button type="submit">Add book</button>
+    <button type="submit">Lägg till bok</button>
 </form>
