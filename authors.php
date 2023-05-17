2105e8a6be3e181d005510ed15bc68b6c96990ec
Niklas Mårdby <niklas.mardby@gmail.com>
2023-05-17 11:00:39 +0200

    Add authors.php

---
+++ authors.php
@@ -0,0 +1,20 @@
+<?php
+
+// require 'classes/book-view.php';
+// require 'classes/book-model.php';
+
+// $pdo = require 'partials/connect.php';
+
+// $bookModel = new BookModel($pdo);
+// $bookView = new BookView();
+
+// ==============================================
+
+include 'partials/header.php';
+include 'partials/nav.php';
+
+// $bookView->renderAllBooksAsList($bookModel->getAllBooks());
+
+// include 'partials/book-form.php';
+
+include 'partials/footer.php';
--- partials/nav.php
+++ partials/nav.php
@@ -1,7 +1,8 @@
 <nav>
-    <a href="index.php">Hem</a> 
+    <a href="index.php">Hem</a>
     <!-- <a href="index.php?page=book">Böcker</a> -->
     <a href="users.php">Bokrejsare</a>
     <a href="books.php">Böcker</a>
+    <a href="authors.php">Författare</a>
     <a href="reviews.php">Recensioner</a>
 </nav>
